8fc8cc78fb5ab9f376d6dd6105e0417736fb2807
tianyishinidie <32159729+tianyishinidie@users.noreply.github.com>
2020-05-22 20:53:07 +0800

    manage_test component: simple css incorrect router-link.to

--- learn_spm/src/components/manage_test/manage_test.vue
+++ learn_spm/src/components/manage_test/manage_test.vue
@@ -1,10 +1,44 @@
 <template>
-    <div></div>
+    <div>
+        <test_info
+                v-for="(testinfo,index) in testsInfo"
+                :test-name="testinfo.testName"
+                :create-date="testinfo.createDate"
+                :complete-num="testinfo.completeNum"
+                :warning-line="testinfo.warningLine"
+                :un-pass-num="testinfo.unPassNum"
+                :key="index"
+        ></test_info>
+    </div>
+
 </template>
 
 <script>
+    import Test_info from "./test_info";
     export default {
-        name: "manage_test"
+        name: "manage_test",
+        // eslint-disable-next-line vue/no-unused-components
+        components: {Test_info},
+        data(){
+            return {
+                testsInfo:[
+                    {
+                        testName: "单元一测试",
+                        createDate:"2020-01-01 12:00:00",
+                        completeNum:35,
+                        warningLine:60,
+                        unPassNum:10,
+                    },
+                    {
+                        testName: "单元二测试",
+                        createDate:"2020-02-01 12:00:00",
+                        completeNum:40,
+                        warningLine:70,
+                        unPassNum:15,
+                    },
+                ],
+            };
+        },
     }
 </script>
 
---
+++ learn_spm/src/components/manage_test/test_info.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="bar">
+        <router-link to="/" tag="div" class="testname">{{testName}}</router-link>
+<!--        <div class="testname">{{testName}}</div>-->
+        <div class="clearfloat"></div>
+        <div>
+            <div class="leftinfo">
+                <span class="iteminfo">{{createDate}}</span>
+                <span class="iteminfo">{{completeNum}}</span>
+            </div>
+            <div class="rightinfo">
+                <span class="iteminfo">{{warningLine}}</span>
+                <span class="iteminfo">{{unPassNum}}</span>
+                <router-link to="/editTest" tag="button">编辑测试</router-link>
+<!--                <button>编辑测试</button>-->
+            </div>
+        </div></div>
+
+</template>
+
+<script>
+    export default {
+        name: "test_info",
+        props: ['testName','createDate','completeNum','warningLine','unPassNum'],
+    }
+</script>
+
+<style scoped>
+    .clearfloat{
+        clear: both;
+    }
+
+    .bar{
+        width:60%;
+        overflow: auto;
+        border:1px solid black;
+        margin:10px auto 10px;
+    }
+
+    .testname{
+        float: left;
+        padding: 0 5px 0 5px;
+        cursor: pointer;
+    }
+
+    .leftinfo{
+        float: left;
+    }
+
+    .rightinfo{
+        float: right;
+    }
+
+    .iteminfo{
+        padding: 0 5px 0 5px;
+    }
+</style>
