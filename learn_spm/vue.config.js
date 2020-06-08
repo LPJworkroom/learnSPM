58c5d29a587ccaa60c5f1100f6432573e4a7244f
LPJworkroom <lpjworkroom@gmail.com>
2020-06-08 09:41:00 +0800

    Update vue.config.js

--- learn_spm/vue.config.js
+++ learn_spm/vue.config.js
@@ -1,15 +1,18 @@
 module.exports = {
     // webpack-dev-server 相关配置
     devServer: {
+        port:80,
         proxy: {
-            '/api': {
-                target: 'http://203.195.198.41/learnSPM',
+            '/api/*': {
+                target: 'http://203.195.198.41/',
                 ws: true,
                 changeOrigin: true,
+                host: 'api.io',
                 pathRewrite: {
-                    '^/api': ''  //通过pathRewrite重写地址，将前缀/api转为/
+                    '/api': ''  //通过pathRewrite重写地址，将前缀/api转为/
                 }
             }
         }
     },
+    publicPath:'',
 }
